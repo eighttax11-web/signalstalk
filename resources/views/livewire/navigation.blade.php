513b9d028aae53e61c33fd81bc08b33a2886131d
Cristian <eighttax11@gmail.com>
2021-03-15 16:18:02 -0600

    Mostrar el listado de posts en la pagina principal, paginarlo, ordenarlos por id de forma ascendente, agregar diseño responsivo

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,5 +1,5 @@
 <nav class="bg-gray-800" x-data="{ open: false }">
-    <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+    <div class="container">
         <div class="relative flex items-center justify-between h-16">
             <!-- Mobile menu button-->
             <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -34,7 +34,7 @@
             </div>
             <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <!-- LOGOTYPE -->
-                <a href="{{ route('welcome') }}" class="flex-shrink-0 flex items-center">
+                <a href="{{ route('posts.index') }}" class="flex-shrink-0 flex items-center">
                     <img class="block lg:hidden h-8 w-auto"
                          src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow">
                     <img class="hidden lg:block h-8 w-auto"
