4f8ffdcb7ca23d477994c2c8dee5d4d8d2dcf971
Cristian <eighttax11@gmail.com>
2021-03-23 22:39:29 -0600

    Traducción parcial de blog, instalación del paquete Laravel/spanish

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -35,7 +35,7 @@
             <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <!-- LOGOTYPE -->
                 <a href="{{ route('posts.index') }}" class="flex-shrink-0 flex items-center">
-                    <h1 class="text-white">SignalsTalk</h1>
+                    <p class="text-white text-bold text-2xl">SignalsTalk</p>
                 </a>
                 <!-- MENU LG -->
                 <div class="hidden sm:block sm:ml-6">
@@ -50,19 +50,6 @@
             </div>
             @auth()
                 <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-
-                    <!-- notifications button -->
-                    <button
-                        class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                        <span class="sr-only">View notifications</span>
-                        <!-- Heroicon name: outline/bell -->
-                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                             stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>
-                        </svg>
-                    </button>
-
                     <!-- Profile dropdown -->
                     <div class="ml-3 relative" x-data="{ open: false }">
                         <div>
@@ -89,18 +76,17 @@
                         <div x-show="open" x-on:click.away="open = false"
                              class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
                              role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
-                            <a href="{{ route('profile.show') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Your
-                                Profile</a>
+                            <a href="{{ route('profile.show') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">{!! __('Your profile') !!}</a>
 
                             @can('admin.home')
-                                <a href="{{ route('admin.home') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Dashboard</a>
+                                <a href="{{ route('admin.home') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">{!! __('Dashboard') !!}</a>
                             @endcan
 
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
                                 <a href="{{ route('logout') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                    Sign out
+                                    {!! __('Sign out') !!}
                                 </a>
                             </form>
                         </div>
@@ -109,9 +95,9 @@
             @else
                 <div>
                     <a href="{{ route('login') }}"
-                       class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Login</a>
+                       class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">{!! __('Login') !!}</a>
                     <a href="{{ route('register') }}"
-                       class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Register</a>
+                       class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">{!! __('Register') !!}</a>
                 </div>
             @endauth
         </div>
